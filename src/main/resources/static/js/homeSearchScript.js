2f4cc771307f8d5fa3f7e07bcc68f0d16227ba2c
mkbrun <matskbrun@hotmail.com>
2024-04-26 15:47:50 +0200

    Added date section to search on homepage, frontend only.

--- src/main/resources/static/js/homeSearchScript.js
+++ src/main/resources/static/js/homeSearchScript.js
@@ -4,7 +4,9 @@ document.addEventListener('DOMContentLoaded', function() {
         searchForm.addEventListener('submit', function(event) {
             event.preventDefault();
             const query = document.getElementById('searchQuery').value;
-            window.location.href = `/search.html?query=${encodeURIComponent(query)}`;
+            const departureDate = document.getElementById('departureDate').value;
+            const returnDate = document.getElementById('returnDate').value;
+            window.location.href = `/search.html?query=${encodeURIComponent(query)}&departure=${encodeURIComponent(departureDate)}&return=${encodeURIComponent(returnDate)}`;
         });
     }
 });
