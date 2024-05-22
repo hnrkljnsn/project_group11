82f9bf06c55af3d63b556779379532a90da0e990
mkbrun <matskbrun@hotmail.com>
2024-05-22 23:38:45 +0200

    Cherry-picked commit where favorite flights are displayed correctly

--- src/main/resources/static/js/favoritesScript.js
+++ src/main/resources/static/js/favoritesScript.js
@@ -38,3 +38,34 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 });
+
+    const apiUrl = `/api/${userId}/favorite-flights`;
+    console.log("API URL:", apiUrl);
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(flights => {
+            const flightList = document.getElementById('favoriteFlightsList');
+            flightList.innerHTML = ''; // Clear existing list items
+            flights.forEach(flight => {
+                const listItem = document.createElement('li');
+                listItem.className = 'flight-card';
+
+                const flightInfo = `
+                    <div class="flight-image">
+                        <img src='https://t4.ftcdn.net/jpg/04/38/64/95/360_F_438649569_DsSHTkasH6GqqQXwu7FbRG0OMHstAc2D.jpg' alt="Flight Image">
+                    </div>
+                    <div class="flight-info" data-header="Airline">${flight.airline}</div>
+                    <div class="flight-info" data-header="Departure">${flight.departureCity}</div>
+                    <div class="flight-info" data-header="Destination">${flight.returnCity}</div>
+                    <div class="flight-info" data-header="Departure Date">${flight.departureDate}</div>
+                    <div class="flight-info" data-header="Return Date">${flight.returnDate}</div>
+                    <div class="flight-info" data-header="Price">${flight.price}</div>
+                `;
+
+                listItem.innerHTML = flightInfo;
+                flightList.appendChild(listItem);
+            });
+        })
+        .catch(error => console.error('Error:', error));
+});
